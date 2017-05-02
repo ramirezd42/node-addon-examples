1682aa6cb2be2f1848a0c7a76fc043e9e4c1c6b1
Dave Ramirez <ramirezd42@gmail.com>
2017-05-02 14:13:36 -0400

    Update addon.js

--- 3_callbacks/nan/addon.js
+++ 3_callbacks/nan/addon.js
@@ -5,11 +5,9 @@ fs.readFile('bye.jpg', renderMustache);
 
 function renderMustache(err, buffer) {
   if (err) throw err;
-  console.log(buffer)
   mustache.renderMustache(buffer, mustacheRendered)
 }
 
 function mustacheRendered(data) {
-  console.log(data)
   console.log('success!')
 }
